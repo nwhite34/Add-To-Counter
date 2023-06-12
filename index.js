74d4068f1cac2c519ec77582f8d705a90c22bd0d
Nick Whiteley <102297051+nwhite34@users.noreply.github.com>
2023-06-12 18:05:15 +1000

    New Database
    Updated the database to fix the error of items not saving to the database.
    Will now save to a new database.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://playground-c91aa-default-rtdb.firebaseio.com/"
+  databaseURL: "https://shopping-list-59003-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(appSettings);
